0578a2a3d8c5e09634687f7492843a95ec2c9ef3
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-09-11 16:57:28 -0400

    background color

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import './App.css'
 import { puppyList } from './data.js'
 //import { useState } from 'react'
+import './App.css'
+import './index.css'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
