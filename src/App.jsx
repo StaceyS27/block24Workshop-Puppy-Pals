60a8bcd43b1860c26d022ac08cdd5b909e80a946
Stacey Sanchez <staceysanchez@macbook-air.mynetworksettings.com>
2023-09-11 16:49:07 -0400

    displaying data from selected pup

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,30 @@ import { puppyList } from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  console.log(puppyList);
-
+  const [featPupId, setfeatPupId] = useState(null);
+  //console.log(puppyList);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
   return (
-      <div>
-        {
-          puppies.map((puppy) => {
-            return <p key ={puppy.id}>{puppy.name}</p>
-          })
-        }
-      </div>
+    <div>
+      {
+        puppies.map((puppy) => {
+          return <p onClick={() => { setfeatPupId(puppy.id) }} key={puppy.id}>{puppy.name}</p>
+        })
+      }
+
+
+      {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2><ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )
+      }
+
+    </div>
   )
 }
 
